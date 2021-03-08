bc7f35e3dbde4220da52be582e14f7e3123c7432
Ryan Pelky <rpelky16@wou.edu>
2021-03-07 19:16:40 -0800

    Implemented home display.

--- Beryl/BerylCalendar/BerylCalendar/Controllers/HomeController.cs
+++ Beryl/BerylCalendar/BerylCalendar/Controllers/HomeController.cs
@@ -7,21 +7,32 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BerylCalendar.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BerylCalendar.Controllers
 {
     public class HomeController : Controller
     {
         private BerylDbContext db;
+        private UserManager<IdentityUser> userManager;
 
-        public HomeController(BerylDbContext db)
+        public HomeController(BerylDbContext db, UserManager<IdentityUser> userManager)
         {
             this.db = db;
+            this.userManager = userManager;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> HomePage()
+        {
+            var events = await db.Events.Where(x => x.Id == Int32.Parse(userManager.GetUserId(User))).OrderBy(y => y.StartTime).ToListAsync();
+            return View(events);
         }
 
         public IActionResult Privacy()
