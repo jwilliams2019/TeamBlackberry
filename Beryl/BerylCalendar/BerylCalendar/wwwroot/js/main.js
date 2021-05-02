7e1f76c58846687c17c1437b01032992555472a7
Jonathan Williams <jwilliams19@mail.wou.edu>
2021-05-02 00:37:04 -0700

    Add new js file, update css, remove prints,add moq

---
+++ Beryl/BerylCalendar/BerylCalendar/wwwroot/js/main.js
@@ -0,0 +1,24 @@
+// function CreateError(a){
+//     var text = "";
+//     if (a.num == 1){
+//         window.alert("Invalid model");
+//     } else {
+//         switch (a.num){
+//             case 0:
+//                 break;
+//             case 2:
+//                 text += "Ending Date and Time are prior to Starting Date and Time\n";
+//                 continue;
+//             default:
+//                 window.alert(text);
+//                 break;
+//         }
+        
+//         window.alert(text);
+//     }
+// }
+
+var displayColors = false;
+
+
+
