3a2ee744e8ef48af502ea1addbe6fc33c67cc4dd
Jonathan Williams <jwilliams19@mail.wou.edu>
2021-05-23 22:21:32 -0700

    Adds password changing feature with email API

--- Beryl/BerylCalendar/BerylCalendar/Controllers/HomeController.cs
+++ Beryl/BerylCalendar/BerylCalendar/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using BerylCalendar.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace BerylCalendar.Controllers
 {
@@ -16,11 +17,13 @@ namespace BerylCalendar.Controllers
     {
         private BerylDbContext db;
         private readonly UserManager<IdentityUser> userManager;
+        private readonly IEmailSender _emailSender;
 
-        public HomeController(BerylDbContext db, UserManager<IdentityUser> userManager)
+        public HomeController(BerylDbContext db, UserManager<IdentityUser> userManager, IEmailSender emailSender)
         {
             this.db = db;
             this.userManager = userManager;
+            _emailSender = emailSender;
         }
 
         public IActionResult Index()
@@ -37,6 +40,52 @@ namespace BerylCalendar.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult PasswordChangeRequest(){
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PasswordChangeRequest(string emailForPass){
+            Debug.WriteLine(emailForPass);
+
+            var callbackUrl = Url.Page(
+                "/Home/PasswordChange/?email="+emailForPass,
+                pageHandler: null,
+                values: new { area = "" },
+                protocol: Request.Scheme);
+            return RedirectToAction("PasswordRequestSent", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult PasswordRquestSent(){
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PasswordChange(string email){
+            var user = await userManager.FindByEmailAsync(email);
+            if (user == null){
+                return RedirectToAction("PasswordChangeRequest");
+            }
+            ViewData["email"] = email;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(string email, string newPassword){
+            var user = await userManager.FindByEmailAsync(email);
+            if (user == null){
+                return RedirectToAction("PasswordChangeRequest");
+            }
+            user.PasswordHash = userManager.PasswordHasher.HashPassword(user, newPassword);
+            var result = await userManager.UpdateAsync(user);
+            if (!result.Succeeded){
+                return RedirectToAction("PasswordChange", "Home", new { email = email });
+            }
+            return RedirectToAction("Index");
         }
     }
 }
